465f1891c0897b90484a5a7ea2c5305237e55f2f
Your Name <you@example.com>
2020-07-21 20:57:34 +0300

    task-6

--- build/js/main/main.js
+++ build/js/main/main.js
@@ -1,114 +1,128 @@
 "use strict";
 
 svg4everybody();
-var arr_cards = [{
-  id: "1",
-  img_src: "img/prod_1.jpg",
-  name: "Кеды SB DELTA FORCEVULC, Nike",
-  quantity: 37,
-  articul: "00674157",
-  price: 3700,
-  arr_color: ["img/color.jpg", "img/color_1.jpg", "img/color_2.jpg"],
-  arr_size: [34, 37, 45]
-}, {
-  id: "2",
-  img_src: "img/prod_2.jpg",
-  name: "Кедsdfsdfsdf sdf",
-  quantity: 34,
-  articul: "00674157",
-  price: 33700
-}, {
-  id: "3",
-  img_src: "img/prod_3.jpg",
-  name: "Кеды SB DELTA FORCEVULC, Nike",
-  quantity: 37,
-  articul: "00674157",
-  price: 3700,
-  arr_color: ["img/color.jpg", "img/color_1.jpg", "img/color_2.jpg"],
-  arr_size: [34, 37, 45]
-}, {
-  id: "4",
-  img_src: "img/prod_4.jpg",
-  name: "Кедsdfsdfsdf sdf",
-  quantity: 34,
-  articul: "00674157",
-  price: 33700
-}];
-var template = document.querySelector("#template-card"),
-    container_cards = document.querySelector(".products-block__tabs-content"),
-    img = template.content.querySelector(".product-card__img"),
-    title = template.content.querySelector(".product-card__title"),
-    quantity = template.content.querySelector(".product-card__quantity-count"),
-    articul = template.content.querySelector(".product-card__articul-text"),
-    price = template.content.querySelector(".product-card__price"),
-    card = template.content.querySelector(".product-card"),
-    stars = template.content.querySelector(".product-card__rating-star"),
-    input_star = stars.querySelectorAll(".rating-star-input"),
-    label_star = stars.querySelectorAll(".rating-star-label"),
-    offers_block = document.createElement("div"),
-    offers_block_title = document.createElement("span"),
-    offers_block_wrap = document.createElement("div"),
-    offer_input = document.createElement("input"),
-    offer_label = document.createElement("label"),
-    offer_img = document.createElement("img"),
-    offer_span = document.createElement("span");
-offers_block.classList.add("product-card__offers-block");
-offers_block_title.classList.add("product-card__offers-title");
-offers_block_wrap.classList.add("product-card__offers-wrap");
-offers_block.append(offers_block_title);
-offers_block.append(offers_block_wrap);
-offer_input.classList.add("product-card__offers-item-input");
-offer_input.type = "radio";
-offer_label.classList.add("product-card__offers-item-label");
-offer_img.classList.add("product-card__offers-item-img");
-offer_span.classList.add("product-card__offers-item-label-text");
-arr_cards.forEach(function (elem) {
-  img.setAttribute("src", elem.img_src);
-  title.textContent = elem.name;
-  quantity.textContent = elem.quantity;
-  articul.textContent = elem.articul;
-  price.textContent = elem.price;
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "http://alex-checktask.devsotbit.ru/teaching/products.php");
+xhr.timeout = 2000;
 
-  for (var i = 0; i < 5; i++) {
-    input_star[i].id = "star-rating_ID" + i + elem.id;
-    input_star[i].name = "star-rating_ID" + elem.id;
-    label_star[i].setAttribute("for", "star-rating_ID" + i + elem.id);
-  }
+xhr.onload = function () {
+  if (xhr.status != 200) {
+    var containerCards = document.querySelector(".products-block__tabs-content");
+    addErrorText(containerCards, "error");
+  } else {
+    var arrCards = JSON.parse(xhr.response);
+    var template = document.querySelector("#template-card"),
+        img = template.content.querySelector(".product-card__img"),
+        title = template.content.querySelector(".product-card__title"),
+        articul = template.content.querySelector(".product-card__articul-text"),
+        price = template.content.querySelector(".product-card__price"),
+        card = template.content.querySelector(".product-card");
 
-  var clone_card = document.importNode(card, true);
-  container_cards.appendChild(clone_card);
-  var form_elem = clone_card.querySelector(".product-card__form"),
-      submit_form_elem = clone_card.querySelector(".product-card__add-to-basket");
+    var _containerCards = document.querySelector(".products-block__tabs-content"),
+        offersBlock = document.createElement("div"),
+        offersBlockTitle = document.createElement("span"),
+        offersBlockWrap = document.createElement("div"),
+        offerInput = document.createElement("input"),
+        offerLabel = document.createElement("label"),
+        offerImg = document.createElement("img"),
+        offerSpan = document.createElement("span");
 
-  if (elem.arr_color) {
-    var color_offers = form_elem.insertBefore(offers_block.cloneNode(true), submit_form_elem),
-        color_offers_title = color_offers.querySelector(".product-card__offers-title"),
-        color_offers_wrap = color_offers.querySelector(".product-card__offers-wrap");
-    color_offers_title.textContent = "Цвет";
-    elem.arr_color.forEach(function (color, num) {
-      var color_input = color_offers_wrap.appendChild(offer_input.cloneNode()),
-          color_label = color_offers_wrap.appendChild(offer_label.cloneNode()),
-          color_img = color_label.appendChild(offer_img.cloneNode());
-      color_input.id = "color_offer_" + elem.id + num;
-      color_input.name = "product-card__offers-color";
-      color_label.setAttribute("for", color_input.id);
-      color_img.setAttribute("src", color);
+    offersBlock.classList.add("product-card__offers-block");
+    offersBlockTitle.classList.add("product-card__offers-title");
+    offersBlockWrap.classList.add("product-card__offers-wrap");
+    offersBlock.append(offersBlockTitle);
+    offersBlock.append(offersBlockWrap);
+    offerInput.classList.add("product-card__offers-item-input");
+    offerInput.type = "radio";
+    offerLabel.classList.add("product-card__offers-item-label");
+    offerImg.classList.add("product-card__offers-item-img");
+    offerSpan.classList.add("product-card__offers-item-label-text");
+    arrCards.forEach(function (elem) {
+      img.setAttribute("src", elem.imgSrc);
+      title.textContent = elem.title;
+      articul.textContent = elem.productCode;
+      price.textContent = elem.price;
+      var cloneCard = document.importNode(card, true);
+
+      _containerCards.appendChild(cloneCard);
+
+      var formElem = cloneCard.querySelector(".product-card__form"),
+          submitFormElem = cloneCard.querySelector(".product-card__add-to-basket"),
+          quantityIconYes = cloneCard.querySelector(".product-card__quantity-icon-in-stock"),
+          quantityIconNo = cloneCard.querySelector(".product-card__quantity-icon-none"),
+          quantity = cloneCard.querySelector(".product-card__quantity-count"),
+          stars = cloneCard.querySelector(".product-card__rating-star"),
+          inputStar = Array.prototype.slice.call(stars.querySelectorAll(".rating-star-input")),
+          labelStar = stars.querySelectorAll(".rating-star-label");
+      inputStar.forEach(function (star, numStar) {
+        star.id = "star-rating_ID" + numStar + elem.id;
+        star.name = "star-rating_ID" + elem.id;
+        labelStar[numStar].setAttribute("for", "star-rating_ID" + numStar + elem.id);
+
+        if (elem.rating == star.value) {
+          star.checked = true;
+        }
+      });
+
+      if (elem.quantity <= 0) {
+        quantity.textContent = 0;
+        quantityIconYes.style.display = "none";
+      } else {
+        quantity.textContent = elem.quantity;
+        quantityIconNo.style.display = "none";
+      }
+
+      if (elem.colors) {
+        var colorOffers = formElem.insertBefore(offersBlock.cloneNode(true), submitFormElem),
+            colorOffersTitle = colorOffers.querySelector(".product-card__offers-title"),
+            colorOffersWrap = colorOffers.querySelector(".product-card__offers-wrap");
+        colorOffersTitle.textContent = "Цвет";
+        elem.colors.forEach(function (color, num) {
+          var colorInput = colorOffersWrap.appendChild(offerInput.cloneNode()),
+              colorLabel = colorOffersWrap.appendChild(offerLabel.cloneNode()),
+              colorImg = colorLabel.appendChild(offerImg.cloneNode());
+          colorInput.id = "color_offer_" + elem.id + num;
+          colorInput.name = "product-card__offers-color";
+          colorInput.value = color.nameColor;
+          colorLabel.setAttribute("for", colorInput.id);
+          colorImg.setAttribute("src", color.imgColor);
+
+          if (num == 0) {
+            colorInput.checked = true;
+          }
+        });
+      }
+
+      if (elem.size) {
+        var sizeOffers = formElem.insertBefore(offersBlock.cloneNode(true), submitFormElem),
+            sizeOffersTitle = sizeOffers.querySelector(".product-card__offers-title"),
+            sizeOffersWrap = sizeOffers.querySelector(".product-card__offers-wrap");
+        sizeOffersTitle.textContent = "Размер";
+        elem.size.forEach(function (size, num) {
+          var sizeInput = sizeOffersWrap.appendChild(offerInput.cloneNode()),
+              sizeLabel = sizeOffersWrap.appendChild(offerLabel.cloneNode()),
+              sizeSpan = sizeLabel.appendChild(offerSpan.cloneNode());
+          sizeInput.id = "size_offer_" + elem.id + num;
+          sizeInput.name = "product-card__offers-size";
+          sizeInput.value = size;
+          sizeLabel.setAttribute("for", sizeInput.id);
+          sizeSpan.textContent = size;
+
+          if (num == 0) {
+            sizeInput.checked = true;
+          }
+        });
+      }
     });
+    removePreloader();
   }
+};
 
-  if (elem.arr_size) {
-    var size_offers = form_elem.insertBefore(offers_block.cloneNode(true), submit_form_elem),
-        size_offers_title = size_offers.querySelector(".product-card__offers-title"),
-        size_offers_wrap = size_offers.querySelector(".product-card__offers-wrap");
-    size_offers_title.textContent = "Размер";
-    elem.arr_size.forEach(function (size, num) {
-      var size_input = size_offers_wrap.appendChild(offer_input.cloneNode()),
-          size_label = size_offers_wrap.appendChild(offer_label.cloneNode()),
-          size_span = size_label.appendChild(offer_span.cloneNode());
-      size_input.id = "size_offer_" + elem.id + num;
-      size_input.name = "product-card__offers-size";
-      size_label.setAttribute("for", size_input.id);
-      size_span.textContent = size;
-    });
-  }
-});
+xhr.ontimeout = function () {
+  var containerCards = document.querySelector(".products-block__tabs-content");
+  addErrorText(containerCards, "timeout");
+  removePreloader();
+};
+
+xhr.send();
+showPreloader();
